793bd17e47e5db3ab42fcab6450342d91042936b
krishnaku <kkumar@exathink.com>
2025-11-26 23:26:25 -0500

    Move html and assets and index to top level of the docs folder.

--- pandocs.sh
+++ pandocs.sh
@@ -8,7 +8,7 @@ shopt -s nullglob
 #   pcalc/docs/*.md              → docs/pandoc/*.html
 #   pcalc/docs/<sub>/*.md        → docs/pandoc/<sub>/*.html
 PAIRS=(
-  "docs/src:docs/site/html"
+  "docs/src:docs/html"
   ".:.local/html"
 )
 
